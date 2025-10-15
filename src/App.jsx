97e5f02b74c4803087cc407f83e6171085528d8e
Greenie115 <dggreen545@gmail.com>
2025-10-15 17:06:42 +0200

    added and animated hero section with text

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { gsap, ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/NAvbar";
+import Hero from "./components/HEro";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <main>
+  return(
+  <main>
     <Navbar />
-  </main>;
-};
+    <Hero />
+    <div className='h-dvh bg-black'></div>
+  </main>
+)};
 
 export default App;
