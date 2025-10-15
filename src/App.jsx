111e186f5f63d09b765aa2085c9b4cc2474463d0
Greenie115 <dggreen545@gmail.com>
2025-10-15 15:26:14 +0200

    navbar added with transparency effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {gsap, ScrollTrigger, SplitText } from "gsap/all";
+import { gsap, ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from "./components/NAvbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <div className="flex-center h-[100vh]">
-      <h1 className='text-3xl text-indigo-500'>Hello World!</h1>
-    </div>
-  );
+  return <main>
+    <Navbar />
+  </main>;
 };
 
 export default App;
