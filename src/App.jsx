f93387e9a1f0b3120aed5845f82b11915fff2665
Greenie115 <dggreen545@gmail.com>
2025-10-17 14:03:51 +0200

    added cocktails section with animationed leaves

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { gsap, ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/NAvbar";
 import Hero from "./components/HEro";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return(
-  <main>
-    <Navbar />
-    <Hero />
-  </main>
-)};
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+    </main>
+  );
+};
 
 export default App;
